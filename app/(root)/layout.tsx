fc34c1be2b5f92c8a9bd00caad836e0d10f00d20
Dylan Truong <trudylan7@gmail.com>
2024-11-12 18:10:56 +1100

    home ui

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,15 @@
+import Sidebar from "../../components/ui/Sidebar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+    const loggedIn = {firstName: "Dylan", lastName: "T" };
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user = {loggedIn}/>
         {children}
     </main>
   );
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import HeaderBox from '../../components/ui/HeaderBox'
+import TotalBalanceBox from '../../components/ui/TotalBalanceBox';
 
 const Home = () => {
+
+    const loggedIn = {firstName: 'Dylan'};
   return (
-    <div>Home</div>
+    <section className = "home">
+        <div className = "home-content">
+            <header className = "home-header">
+                <HeaderBox 
+                type = "greeting"
+                title = "Welcome"
+                user = {loggedIn?.firstName || "Guest"}
+                subtext = "Access manage your accounts and transactions"
+                
+                />
+            <TotalBalanceBox 
+                accounts={[]}
+                totalBanks={1}
+                totalCurrentBalance={1250}
+            />
+            </header>
+        </div>
+    
+    
+    
+    </section>
+
   )
 }
 
